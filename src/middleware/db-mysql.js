59845cfeeff0cb9b6b496f19d75aed9bd8ea90ff
chandra bachtiar <chandrabahtiar25@gmail.com>
2022-11-25 23:27:21 +0700

    [change] some dump action

--- src/middleware/db-mysql.js
+++ src/middleware/db-mysql.js
@@ -2,16 +2,10 @@ const mysql = require("mysql2/promise");
 
 async function connection() {
     const con = await mysql.createConnection({
-        // host: process.env.MYSQL_HOST,
-        // user: process.env.MYSQL_USER,
-        // password: process.env.MYSQL_PASSWORD,
-        // database: process.env.MYSQL_DB,
-        // waitForConnections: true,
-        // connectionLimit: process.env.MYSQL_CON_LIMIT,
-        host: "103.176.78.64",
-        user: "root",
-        password: "404:NotFound",
-        database: "BOT",
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DB,
     });
     return con;
 }
